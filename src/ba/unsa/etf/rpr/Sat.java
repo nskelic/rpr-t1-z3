77f7f573e80d737c343e045a28a0d397b8cefeee
nskelic <nskelic1@etf.unsa.ba>
2020-10-30 16:44:25 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr;
+
+
+public class Sat {
+    int sati,minute,sekunde;
+
+    public Sat(int sati, int minute, int sekunde) { Postavi(sati,minute,sekunde); }
+    public void Postavi(int h, int m, int s) { sati=h; minute=m; sekunde=s; }
+    public void Sljedeci() {
+        sekunde++;
+        if (sekunde==60) { sekunde=0; minute++; }
+        if (minute==60) { minute=0; sati++; }
+        if (sati==24) sati=0;
+    }
+    public void Prethodni() {
+        sekunde--;
+        if (sekunde==-1) { sekunde=59; minute--; }
+        if (minute==-1) { minute=59; sati--; }
+        if (sati==-1) sati=23;
+    }
+    public void PomjeriZa(int pomak) {
+        if (pomak>0) for (int i=0; i<pomak; i++) Sljedeci();
+		else for (int i=0; i<-pomak; i++) Prethodni();
+    }
+    public int DajSate() { return sati; }
+    public int DajMinute() { return minute; }
+    public int DajSekunde() { return sekunde; }
+    public void Ispisi() { System.out.println(sati + ":" + minute + ":" + sekunde + "\n"); }
+};
